45b1c9cebb79f8db94a5c8e1a58dc37397985cb0
Brnn <juniobrennon@gmail.com>
2024-01-06 11:30:03 -0400

    feat: GetOrders Finalizada

--- Models/OrderResponse.cs
+++ Models/OrderResponse.cs
@@ -5,19 +5,18 @@ using System.ComponentModel.DataAnnotations.Schema;
 public class OrderResponse
 {
     public string order { get; set; }
-    public decimal  quantity { get; set; }
+    public decimal quantity { get; set; }
     public string productCode { get; set; }
-        public string productDescription { get; set; } 
-        public string image { get; set; } 
-        public decimal cycleTime { get; set; }
+    public string productDescription { get; set; }
+    public string image { get; set; }
+    public decimal cycleTime { get; set; }
+    public List<MaterialResponse> materials { get; set; }
 
+    public OrderResponse() {}
+}
 
-    public OrderResponse() { }
-
-    public OrderResponse(string order, decimal  quantity, string productCode, ProductModel product)
-    {
-        this.order = order;
-        this.quantity = quantity;
-        this.productCode = productCode;
-    }
+public class MaterialResponse
+{
+    public string materialCode { get; set; }
+    public string materialDescription { get; set; }
 }
--- Service/Orders/Order.Service.cs
+++ Service/Orders/Order.Service.cs
@@ -15,62 +15,86 @@ namespace OrdersService
             this.db = db;
         }
 
-  public async Task<ApiResponse<List<OrderResponse>>> GetOrders()
-    {
-        ApiResponse<List<OrderResponse>> response;
-
-        try
+        public async Task<ApiResponse<List<OrderResponse>>> GetOrders()
         {
-            var query =
-                from order in db.Order
-                join product in db.Product on order.productCode equals product.productCode
-                select new
-                {
-                    order.order,
-                    order.quantity,
-                    product.productCode,
-                    product.image,
-                    product.cycleTime,
-                    product.productDescription
-                };
+            ApiResponse<List<OrderResponse>> response;
 
-            var result = await query.ToListAsync();
-
-           var orders = result
-            .Select(
-                item =>
-                    new OrderResponse
+            try
+            {
+                var query =
+                    from order in db.Order
+                    join product in db.Product on order.productCode equals product.productCode
+                    join productMaterial in db.ProductMaterial
+                        on product.productCode equals productMaterial.productCode
+                    join materials in db.Material
+                        on productMaterial.materialCode equals materials.materialCode
+                    orderby order.order
+                    select new
                     {
-                        order = item.order,
-                        quantity = item.quantity,
-                        productCode = item.productCode,
-                        productDescription = item.productDescription, // Adicione esta linha
-                        image = item.image, // Adicione esta linha
-                        cycleTime = item.cycleTime // Adicione esta linha
-                    }
-            )
-            .ToList();
+                        order.order,
+                        order.quantity,
+                        product.productCode,
+                        product.image,
+                        product.cycleTime,
+                        product.productDescription,
+                        materials.materialCode,
+                        materials.materialDescription
+                    };
 
+                var result = await query.ToListAsync();
 
-            response = new ApiResponse<List<OrderResponse>>(
-                "200",
-                "S",
-                "Ordens Listadas com sucesso!",
-                orders
+                var orders = result
+                    .GroupBy(
+                        item =>
+                            new
+                            {
+                                item.order,
+                                item.quantity,
+                                item.productCode,
+                                item.productDescription,
+                                item.image,
+                                item.cycleTime
+                            }
+                    )
+                    .Select(
+                        groupedItem =>
+                            new OrderResponse
+                            {
+                                order = groupedItem.Key.order,
+                                quantity = groupedItem.Key.quantity,
+                                productCode = groupedItem.Key.productCode,
+                                productDescription = groupedItem.Key.productDescription,
+                                image = groupedItem.Key.image,
+                                cycleTime = groupedItem.Key.cycleTime,
+                                materials = groupedItem
+                                    .Select(
+                                        materialItem =>
+                                            new MaterialResponse
+                                            {
+                                                materialCode = materialItem.materialCode,
+                                                materialDescription =
+                                                    materialItem.materialDescription
+                                            }
+                                    )
+                                    .ToList()
+                            }
+                    )
+                    .ToList();
+
+                response = new ApiResponse<List<OrderResponse>>(
+                    "200",
+                    "S",
+                    "Ordens Listadas com sucesso!",
+                    orders
                 // orders
-            );
-        }
-        catch (Exception ex)
-        {
-            response = new ApiResponse<List<OrderResponse>>(
-                "201",
-                "E",
-                ex.Message,
-                null
-            );
-        }
+                );
+            }
+            catch (Exception ex)
+            {
+                response = new ApiResponse<List<OrderResponse>>("201", "E", ex.Message, null);
+            }
 
-        return response;
+            return response;
+        }
     }
-}
 }
