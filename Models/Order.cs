a18e37778c3fbd92eeaa38439d946db854b9b645
Brnn <juniobrennon@gmail.com>
2024-01-05 20:51:17 -0400

    fix: product

--- Models/Order.cs
+++ Models/Order.cs
@@ -4,20 +4,24 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 public class OrderModel
 {
-	[Key]
-	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-	public string order { get; set; }
-	public float quantity { get; set; }
-	public string productCode { get; set; }
-	public OrderModel(
-		string order,
-		float quantity,
-		string productCode
-	)
-	{
-		this.order = order;
-		this.quantity = quantity;
-		this.productCode = productCode;
-	}
-}
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public string order { get; set; }
+    public float quantity { get; set; }
+    public string productCode { get; set; }
 
+    // Propriedade de navegação
+    [ForeignKey("productCode")]
+    public ProductModel product { get; set; }
+
+    // Construtor sem parâmetros necessário para o Entity Framework
+    public OrderModel() { }
+
+    public OrderModel(string order, float quantity, string productCode, ProductModel product)
+    {
+        this.order = order;
+        this.quantity = quantity;
+        this.productCode = productCode;
+        this.product = product;
+    }
+}
